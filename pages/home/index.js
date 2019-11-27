929071f4c7df096a6ad1b362a783485396acd56d
BETH-zhang <522718277@qq.com>
2019-11-27 16:45:39 +0800

    完成Page和Component之间的数据切换

--- pages/home/index.js
+++ pages/home/index.js
@@ -73,9 +73,11 @@ Component({
       })
     },
     NavChange(e) {
-      this.setData({
+      var myEventDetail = {
         PageCur: e.currentTarget.dataset.cur,
-      })
+      } // detail对象，提供给事件监听函数
+      var myEventOption = {} // 触发事件的选项
+      this.triggerEvent('myevent', myEventDetail, myEventOption)
     },
     setGapItemsDefault() {
       const width = this.systemInfo.windowWidth
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,10 @@ Page({
   onLoad() {
     console.log('basics', this.data.PageCur)
   },
+  myEventListener(e) {
+    console.log('e.detail: ', e)
+    this.setData(e.detail)
+  },
   NavChange(e) {
     this.setData({
       PageCur: e.currentTarget.dataset.cur,
