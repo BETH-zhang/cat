2981ac8eef0200dd55dae68cc782f1ba780772b6
BETH-zhang <522718277@qq.com>
2019-11-29 17:11:45 +0800

    更新图标库

--- pages/about/work/index.js
+++ pages/about/work/index.js
@@ -1,28 +1,77 @@
 const app = getApp();
-Page({
+
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  properties: {
+    role: { type: 'String', value: '' },
+  },
   data: {
     works: [],
   },
-  onLoad: function () {
-    this.initData()
+  attached() {
+    console.log('role', this.data.role)
+    if (this.data.role === 'master') {
+      this.initMasterData()
+    } else {
+      this.initData()
+    }
   },
-  pageBack() {
-    wx.navigateBack({
-      delta: 1
-    });
-  },
-  initData() {
-    var works = wx.getStorageSync('myWork') || []
-    const urls = works.map((item) => item && item.path)
-    this.setData({
-      works,
-      urls,
-    })
-  },
-  viewImages(e) {
-    wx.previewImage({
-      urls: this.data.urls,
-      current: e.currentTarget.dataset.cur,
-    })
+  methods: {
+    pageBack() {
+      wx.navigateBack({
+        delta: 1
+      });
+    },
+    initMasterData() {
+      
+    },
+    initData() {
+      var works = wx.getStorageSync('myWork') || []
+      const urls = works.map((item) => item && item.path)
+      this.setData({
+        works,
+        urls,
+      })
+    },
+    viewImages(e) {
+      wx.previewImage({
+        urls: this.data.urls,
+        current: e.currentTarget.dataset.cur,
+      })
+    }
   }
-});
+})
+
+// Page({
+//   properties: {
+//     role: { type: 'String', value: '' },
+//   },
+//   data: {
+//     works: [],
+//   },
+//   onLoad: function () {
+//     console.log('role', this.data.role)
+//     this.initData()
+//   },
+//   pageBack() {
+//     wx.navigateBack({
+//       delta: 1
+//     });
+//   },
+//   initData() {
+//     var works = wx.getStorageSync('myWork') || []
+//     const urls = works.map((item) => item && item.path)
+//     this.setData({
+//       works,
+//       urls,
+//     })
+//   },
+//   viewImages(e) {
+//     wx.previewImage({
+//       urls: this.data.urls,
+//       current: e.currentTarget.dataset.cur,
+//     })
+//   }
+// });
