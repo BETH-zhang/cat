d176b438dcbe3fec88e023125aeab5219cf9b1c4
BETH-zhang <522718277@qq.com>
2019-11-29 17:22:34 +0800

    上传json文件

--- pages/about/work/index.js
+++ pages/about/work/index.js
@@ -25,7 +25,19 @@ Component({
       });
     },
     initMasterData() {
-      
+      console.log('initMasterData: ')
+      wx.request({
+        url: 'http://zhongguose.com/colors.json', //这里填写你的接口路径
+        header: { //这里写你借口返回的数据是什么类型，这里就体现了微信小程序的强大，直接给你解析数据，再也不用去寻找各种方法去解析json，xml等数据了
+         'Content-Type': 'application/json'
+        },
+        data: {//这里写你要请求的参数
+        },
+        success: function(res) {
+          //这里就是请求成功后，进行一些函数操作
+          console.log('res.data: ', res.data)
+        }
+       })
     },
     initData() {
       var works = wx.getStorageSync('myWork') || []
--- utils/util.js
+++ utils/util.js
@@ -74,6 +74,18 @@ const throttle = (method, delay, duration) => {
 
 const fetchApi = () => {
   // http://zhongguose.com/colors.json
+  wx.request({
+    url: 'http://zhongguose.com/colors.json', //这里填写你的接口路径
+    header: { //这里写你借口返回的数据是什么类型，这里就体现了微信小程序的强大，直接给你解析数据，再也不用去寻找各种方法去解析json，xml等数据了
+     'Content-Type': 'application/json'
+    },
+    data: {//这里写你要请求的参数
+    },
+    success: function(res) {
+      //这里就是请求成功后，进行一些函数操作
+      console.log(res.data)
+    }
+   })
 }
 
 module.exports = {
